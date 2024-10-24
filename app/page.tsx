b1843cd852db46adc1d474fac7621355d2a9dd6f
linekersouza3 <linekersouza.s@hotmail.com>
2024-10-23 21:25:23 -0300

    feat: add Booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header";
 import { Input } from "./_components/ui/input";
 import { Button } from "./_components/ui/button";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 const Home = () => {
   return ( 
@@ -23,6 +26,26 @@ const Home = () => {
           <Image alt="Agende com os melhores no HoopPlace" src="/banner-01.png" fill className="rounded-xl object-cover" />
 
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png"/>
+                </Avatar>
+                <p className="text-sm">Barbearia do João</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Outubro</p>
+              <p className="text-2xl">23</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
