fc71faf0fbbeecbad62b641b93ba53de63417416
linekersouza3 <linekersouza.s@hotmail.com>
2024-11-08 20:50:07 -0300

    feat: add sidebar

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,69 @@
-import { MenuIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { quickSearchOptions } from "../_constants/search";
+import { Avatar } from "./ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import Link from "next/link";
 
 const Header = () => {
     return ( 
         <Card>
             <CardContent className="p-5 flex flex-row items-centers justify-between">
                 <Image alt="Logo Hoop" src="/logo.jpg" height={18} width={120}/>
-                <Button size="icon" variant="outline">
-                    <MenuIcon/>
-                </Button>
+                <Sheet> 
+                    <SheetTrigger asChild>
+                        <Button size="icon" variant="outline">
+                            <MenuIcon/>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent className="overflow-y-auto">
+                        <SheetHeader>
+                            <SheetTitle className="text-left">Menu</SheetTitle>
+                        </SheetHeader>
+                        <div className="py-5 border-b border-solid flex items-center gap-3">
+                            <Avatar>
+                                <AvatarImage src="https://plus.unsplash.com/premium_photo-1691784781482-9af9bce05096?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></AvatarImage>
+                            </Avatar>
+                            <div>
+                                <p className="font-bold">Juciane Maluca</p>
+                                <p className="text-xs">juciane@mail.com</p>
+                            </div>
+                        </div>
+                        <div className="py-5 flex flex-col gap-2 border-b border-solid">
+                            <SheetClose asChild>
+                                <Button className="gap-2 justify-start" variant="ghost" asChild>
+                                    <Link href="/">
+                                        <HomeIcon size={18}/>
+                                        Início
+                                    </Link>
+                                </Button>
+                            </SheetClose>
+                            <Button className="gap-2 justify-start" variant="ghost">
+                                <CalendarIcon size={18}/>
+                                Agendamentos
+                            </Button>
+                        </div>
+
+                        <div className="py-5 flex flex-col gap-2 border-b border-solid">
+                            {quickSearchOptions.map(option => (
+                                <Button key={option.title} className="gap-2 justify-start" variant="ghost">
+                                    <Image alt={option.title} src={option.imageUrl} height={18} width={18}/>
+                                    {option.title}
+                                </Button>
+                            ))}
+                        </div>
+
+                        <div className="py-5 flex flex-col gap-2">
+                            <Button className="gap-2 justify-start" variant="ghost">
+                                <LogOutIcon size={18}/>
+                                Sair da conta
+                            </Button>
+                        </div>
+                    </SheetContent>
+                </Sheet>
             </CardContent>
         </Card>
      );
