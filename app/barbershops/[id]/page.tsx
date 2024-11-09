957e9c26913ae7101bb0a556321cec9adbe4c9bc
linekersouza3 <linekersouza.s@hotmail.com>
2024-11-08 21:03:21 -0300

    feat: add sideBar Componeent

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import PhoneItem from "@/app/_components/phone-item";
 import ServiceItem from "@/app/_components/service-item";
+import SideBarSheet from "@/app/_components/sidebar-sheet";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
@@ -35,9 +37,14 @@ const BarbershopPage = async ({params} : BarberShopProps) => {
                     <ChevronLeftIcon />
                 </Link>
             </Button>
-            <Button size="icon" variant="secondary" className="absolute top-4 right-4" asChild>
-                <MenuIcon />
-            </Button>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline" className="absolute top-4 right-4">
+                        <MenuIcon/>
+                    </Button>
+                </SheetTrigger>
+                <SideBarSheet/>
+            </Sheet>
         </div>
 
         <div className="p-5 border-b border-solid">
